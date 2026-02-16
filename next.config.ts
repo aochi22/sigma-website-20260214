74ede21b4b03208f7afb4b3caa30bb6858dddf54
2390727 <2390727@bdeb.qc.ca>
2026-02-15 21:17:43 -0500

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 import path from "node:path";
-const loaderPath = require.resolve('orchids-visual-edits/loader.js');
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -15,21 +13,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [loaderPath]
-      }
-    }
-  }
 };
 
 export default nextConfig;
-// Orchids restart: 1770170516564
